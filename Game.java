24dc6361479da3679432e9407474297c548052aa
GalaxyJumper <oliverdvis@outlook.com>
2024-09-17 09:01:52 -0700

    Game fixed

--- Game.java
+++ Game.java
@@ -1,11 +1,13 @@
+import java.awt.AWTException;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Timer;
+import javax.swing.Timer;
 
 public class Game implements ActionListener{
     Timer gameTimer;
-    public Game() {
-        gameTimer = new Timer(17, this);
+    public Game() throws AWTException{
+        gameTimer = new Timer(11, this);
+        gameTimer.start();
     }
     @Override
     public void actionPerformed(ActionEvent e) {
