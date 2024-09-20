98388d43acdf6975bc9fe2edd0a0bab2824e6018
199No <Nobear08@outlook.com>
2024-09-20 15:42:14 -0700

    Goofy ah fire alarm went off so Im leaving some of it commented

--- Sounds.java
+++ Sounds.java
@@ -14,45 +14,167 @@ public class Sounds {
     Clip clipLevelUp;
     Clip clipCarnival;
 
-    //When a sound is made take the filePath
-    public Sounds(){
+    //Dont do anything when a sound is created
+    public Sounds(){} 
+/*
+    public void Test3Play(){
+        try {
+            
+            //make the SoundFile Var = the SoundFile Given
+            File soundFileLevelUp = new File("Sounds\\CARNIVAL(KanyeWest).wav");
+            
+            //Get the Audio from the Soundfile
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(soundFileLevelUp);
 
+            //Make the clip equal to the Audio
+            clipCarnival = AudioSystem.getClip(); // Create a new Clip for each sound
+            
+            //Open the Clip
+            clipCarnival.open(audioInputStream);
+
+            //Play the clip of that sound
+            clipCarnival.start();
         } 
+        //Cacth for the program
+        catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
 
+            //If something hapeens print it out
+            e.printStackTrace();
 
-
-public void CarnivalPlay(){
-    try {
-        
-        //make the SoundFile Var = the SoundFile Given
-        File soundFileLevelUp = new File("Sounds\\CARNIVAL(KanyeWest).wav");
-        
-        //Get the Audio from the Soundfile
-        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(soundFileLevelUp);
-
-        //Make the clip equal to the Audio
-        clipCarnival = AudioSystem.getClip(); // Create a new Clip for each sound
-        
-        //Open the Clip
-        clipCarnival.open(audioInputStream);
-
-        //Play the clip of that sound
-        clipCarnival.start();
-    } 
-    //Cacth for the program
-    catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
-
-        //If something hapeens print it out
-        e.printStackTrace();
-
+        }
     }
-}
-public void CarnivalStop() {
-    clipCarnival.flush();
-    clipCarnival.close(); 
-}
+    public void Test3Stop() {
+        clipCarnival.flush();
+        clipCarnival.close(); 
+    }
+
+    
+    public void Test3Play(){
+        try {
+            
+            //make the SoundFile Var = the SoundFile Given
+            File soundFileLevelUp = new File("Sounds\\CARNIVAL(KanyeWest).wav");
+            
+            //Get the Audio from the Soundfile
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(soundFileLevelUp);
+
+            //Make the clip equal to the Audio
+            clipCarnival = AudioSystem.getClip(); // Create a new Clip for each sound
+            
+            //Open the Clip
+            clipCarnival.open(audioInputStream);
+
+            //Play the clip of that sound
+            clipCarnival.start();
+        } 
+        //Cacth for the program
+        catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+
+            //If something hapeens print it out
+            e.printStackTrace();
+
+        }
+    }
+    public void Test3Stop() {
+        clipCarnival.flush();
+        clipCarnival.close(); 
+    }
+ */
+
+    public void Test2Play(){
+        try {
+            
+            //make the SoundFile Var = the SoundFile Given
+            File soundFileLevelUp = new File("Sounds\\CARNIVAL(KanyeWest).wav");
+            
+            //Get the Audio from the Soundfile
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(soundFileLevelUp);
+
+            //Make the clip equal to the Audio
+            clipCarnival = AudioSystem.getClip(); // Create a new Clip for each sound
+            
+            //Open the Clip
+            clipCarnival.open(audioInputStream);
+
+            //Play the clip of that sound
+            clipCarnival.start();
+        } 
+        //Cacth for the program
+        catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+
+            //If something hapeens print it out
+            e.printStackTrace();
+
+        }
+    }
+    public void Test2Stop() {
+        clipCarnival.flush();
+        clipCarnival.close(); 
+    }
 
 
+    public void Test1Play(){
+        try {
+            
+            //make the SoundFile Var = the SoundFile Given
+            File soundFileLevelUp = new File("Sounds\\CARNIVAL(KanyeWest).wav");
+            
+            //Get the Audio from the Soundfile
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(soundFileLevelUp);
+
+            //Make the clip equal to the Audio
+            clipCarnival = AudioSystem.getClip(); // Create a new Clip for each sound
+            
+            //Open the Clip
+            clipCarnival.open(audioInputStream);
+
+            //Play the clip of that sound
+            clipCarnival.start();
+        } 
+        //Cacth for the program
+        catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+
+            //If something hapeens print it out
+            e.printStackTrace();
+
+        }
+    }
+    public void Test1Stop() {
+        clipCarnival.flush();
+        clipCarnival.close(); 
+    }
+
+
+    public void CarnivalPlay(){
+        try {
+            
+            //make the SoundFile Var = the SoundFile Given
+            File soundFileLevelUp = new File("Sounds\\CARNIVAL(KanyeWest).wav");
+            
+            //Get the Audio from the Soundfile
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(soundFileLevelUp);
+
+            //Make the clip equal to the Audio
+            clipCarnival = AudioSystem.getClip(); // Create a new Clip for each sound
+            
+            //Open the Clip
+            clipCarnival.open(audioInputStream);
+
+            //Play the clip of that sound
+            clipCarnival.start();
+        } 
+        //Cacth for the program
+        catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+
+            //If something hapeens print it out
+            e.printStackTrace();
+
+        }
+    }
+    public void CarnivalStop() {
+        clipCarnival.flush();
+        clipCarnival.close(); 
+    }
 
 
     public void LevelUpPlay(){
