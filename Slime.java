aa4b585e8254f6d07bfc5862d44e764e12f4ce38
Anders0926 <1079942@apps@nsd.org>
2024-10-01 08:39:56 -0700

    new file:   Slime.java

---
+++ Slime.java
@@ -0,0 +1,43 @@
+//-------------------------------------------------//
+//                    Imports                      //
+//-------------------------------------------------// 
+
+//-------------------------------------------------//
+//                   Enemies                       //
+//-------------------------------------------------// 
+public class Slime extends Enemies{
+    ///////////////
+    //Properties
+    ///////////////
+    
+    ///////////////
+    //Constuctor
+    //////////////
+    public Slime(){
+        super();
+        this.health = 10;
+        this.damage = 1;
+        this.speed = 10;
+        this.xPos = 10;
+        this.yPos = 10;
+        System.out.println("Enemies!");
+    }
+
+    //-------------------------------------------------//
+    //                    Methods                      //
+    //-------------------------------------------------// 
+
+    //moves towards player by 1 tile (both x and y possibily)
+    public void moveTowardsPlayer(int playerX, int playerY){
+        if(this.xPos < playerX){
+            xPos++;
+        }else if(this.xPos > playerX){
+            xPos--;
+        }
+        if(this.yPos > playerY){
+            yPos--;
+        }else if(this.yPos < playerY){
+            yPos++;
+        }
+    }
+}
