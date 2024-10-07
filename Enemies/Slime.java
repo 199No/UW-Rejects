c6a15b7d20371f3905e95ee4e44d9bdcd210959f
Anders0926 <1079942@apps@nsd.org>
2024-10-07 08:48:57 -0700

    modified:   Enemies/Enemies.java 	modified:   Enemies/Slime.java 	renamed:    ActionPerformer.java -> src/ActionPerformer.java 	renamed:    Game.java -> src/Game.java 	renamed:    GraphicsRunnable.java -> src/GraphicsRunnable.java 	renamed:    Gui.java -> src/Gui.java 	renamed:    Input.java -> src/Input.java 	renamed:    Main.java -> src/Main.java 	renamed:    Player.java -> src/Player.java 	renamed:    Sounds.java -> src/Sounds.java

--- Enemies/Slime.java
+++ Enemies/Slime.java
@@ -1,4 +1,5 @@
 package Enemies;
+import src.Player;
 
 public class Slime extends Enemies{
     ///////////////
@@ -25,16 +26,51 @@ public class Slime extends Enemies{
     //-------------------------------------------------// 
 
     //moves towards player by 1 tile (both x and y each)
-    public void moveTowardsPlayer(int playerX, int playerY){
-        if(this.xPos < playerX){
+    //does this when alerted
+    public void moveTowardsPlayer(Player player){
+        if(this.xPos < player.getxPos()){
             xPos++;
-        }else if(this.xPos > playerX){
+        }else if(this.xPos > player.getxPos()){
             xPos--;
         }
-        if(this.yPos > playerY){
+        if(this.yPos > player.getyPos()){
             yPos--;
-        }else if(this.yPos < playerY){
+        }else if(this.yPos < player.getyPos()){
             yPos++;
         }
+    }
+
+    //checks if the slimes position is close to player "alerts" slime when close to
+    public void lookForPlayer(Player player){
+        int slimeX = this.getxPos();
+        int slimeY = this.getyPos();
+        int playerX = player.getxPos();
+        int playerY = player.getyPos();
+        if(slimeX < playerX){
+            if(slimeY < playerY){
+                //bigger playX & playY
+                if(playerX - slimeX < 5 || playerY - slimeY < 5){
+                    this.alert = true;
+                }
+            }else{
+                //bigger playX & slimeY
+                if(playerX - slimeX < 5 || slimeY - playerY < 5){
+                    this.alert = true;
+                }
+            }
+        }else{
+            if(slimeY < playerY){
+                //bigger slimeX & playerY
+                if(slimeX - playerX < 5 || playerY - slimeY < 5){
+                    this.alert = true;
+                }
+            }else{
+                //bigger slimeX & slimeY
+                if(slimeX - playerX < 5 || slimeY - playerY < 5){
+                    this.alert = true;
+                }
+            }
+        }
+
     }
 }
