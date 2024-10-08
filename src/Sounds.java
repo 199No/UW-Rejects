91e7ae35e41084e255d82762c14fe327b736ceb5
199No <Nobear08@outlook.com>
2024-10-08 08:52:20 -0700

    Added the constructur

--- src/Sounds.java
+++ src/Sounds.java
@@ -52,6 +52,36 @@ public class Sounds{
             clipBangaudioInputStream = AudioSystem.getAudioInputStream(new File("Sounds\\Bang.wav"));
             clipBang = AudioSystem.getClip();
             
+            clipCarnivalaudioInputStream = AudioSystem.getAudioInputStream(new File("Sounds\CARNIVAL(KanyeWest).wav"));
+            clipCarnival = AudioSystem.getClip();
+
+            clipCountrysideaudioInputStream = AudioSystem.getAudioInputStream(new File("Sounds\Countryside.wav"));
+            clipCountryside = AudioSystem.getClip();
+
+            clipDeathaudioInputStream = AudioSystem.getAudioInputStream(new File("Sounds\Death.wav"));
+            clipDeath = AudioSystem.getClip();
+
+            clipForestWalkaudioInputStream = AudioSystem.getAudioInputStream(new File("Sounds\ForestWalk.wav"));
+            clipForestWalk = AudioSystem.getClip();
+
+            clipGameboySoundaudioInputStream = AudioSystem.getAudioInputStream(new File("Sounds\GameboySound.wav"));
+            clipGameboySound = AudioSystem.getClip();
+
+            clipLevelUpaudioInputStream = AudioSystem.getAudioInputStream(new File("Sounds\LevelUp.wav"));
+            clipLevelUp = AudioSystem.getClip();
+
+            clipMoneyChingaudioInputStream = AudioSystem.getAudioInputStream(new File("Sounds\MoneyChing.wav"));
+            clipMoneyChing = AudioSystem.getClip();
+
+            clipRainForestaudioInputStream = AudioSystem.getAudioInputStream(new File("Sounds\RainForest.wav"));
+            clipRainForest = AudioSystem.getClip();
+
+            clipSwordAttackaudioInputStream = AudioSystem.getAudioInputStream(new File("Sounds\SwordAttack.wav"));
+            clipSwordAttack = AudioSystem.getClip();
+
+            clipWalkingaudioInputStream = AudioSystem.getAudioInputStream(new File("Sounds\Walking.wav"));
+            clipWalking = AudioSystem.getClip();
+
         }  
 
 
