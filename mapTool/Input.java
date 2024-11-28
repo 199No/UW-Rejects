8b918e2a417f935e97954ec01b33709f23136108
GalaxyJumper <oliverdvis@outlook.com>
2024-11-27 19:32:20 -0800

    cleanup

--- mapTool/Input.java
+++ mapTool/Input.java
@@ -1,5 +1,6 @@
 package mapTool;
-
+// oh yeah I stole most of this code from anders
+// most of the credit goes to him (a small amount of this is my code)
 
 //pressed -> released (check if short)  [100 - 350]
 //time inbetween
