e520caf00acbfaaf00dbcfbc2af16d04a0567a37
GalaxyJumper <oliverdvis@outlook.com>
2025-02-10 08:37:11 -0800

    soudns mysteries

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException, AWTException{
-        new Sounds().playSound("anvilHit");
+        new Sounds().playSound("AnvilHit");
         
         new Game();
     }
