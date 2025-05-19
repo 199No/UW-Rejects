7fcdb593d210a97ce1dd479749bb4cedec8b5954
Anders0926 <1079942@apps@nsd.org>
2025-05-19 09:00:54 -0700

    monsta monsta monsta

--- Enemies/Monster.java
+++ Enemies/Monster.java
@@ -1,6 +1,11 @@
 package Enemies;
+import src.Entity;
+import java.util.ArrayList;
+import src.Player;
+import java.util.Vector;
+import java.awt.Rectangle;
 
-public class Monster {
+public abstract class Monster extends Entity {
 
     double speed;
     double range;
@@ -14,26 +19,33 @@ public class Monster {
         dead
     }
 
-    public Monster(){
+    private Vector<Double> velocity;
+
+    public Monster(double x, double y, double width, double height, Rectangle hitbox){
+        super(x,y,width,height,hitbox);
         state = State.idle;
+        this.velocity = new Vector<>(2);
     }
 
-    public void update(){
+    public void update(ArrayList<Player> players){
+        int eyesight = 200;
         switch(state){
             case idle:
                 System.out.println("Idle!");
+                idle();
                 break;
             case chasing:
                 System.out.println("Chasing!");
+                chase(players);
                 break;
             case dead:
-                System.out.println("dead!");
+                //System.out.println("dead!");
                 break;
         }
-        if(inRange() && this.state != State.dead){
+        if(inRange(players, eyesight) && this.state != State.dead){
             this.state = State.chasing;
         }
-        else if(!inRange() && this.state != State.dead){
+        else if(!inRange(players, eyesight) && this.state != State.dead){
             this.state = State.idle;
         }
     }
@@ -41,17 +53,28 @@ public class Monster {
     public void updateState(State state){
         this.state = state;
     }
-    //TODO: Figure out if plater is in range
-    public boolean inRange(){
+
+    public boolean inRange(ArrayList<Player> players, int eyesight){
+        for(int i = 0; i < players.size(); i++){
+            // Calculate the distance to the player's location
+            double dx = players.get(i).getX() - this.getX();
+            double dy = players.get(i).getY() - getY();
+            double distance = Math.sqrt(dx * dx + dy * dy);
+            
+            if(distance <= eyesight){
+                return true;
+            }
+        }
         return false;
     }
 
     public void idle(){
 
     }
-    //TODO: Figure out how to use vectors to advantage
-    public void chase(){
 
+    //TODO: Figure out how to use vectors to advantage
+    public void chase(ArrayList<Player> Players){
+        
     }
 
     public void die(){
@@ -61,5 +84,10 @@ public class Monster {
     public boolean getAlive(){
         return this.alive;
     }
-    
+
+    /* 
+    public BufferedImage getShadowImage(){
+        return Gui.toShadow(getImage());
+    }
+    */
 }
